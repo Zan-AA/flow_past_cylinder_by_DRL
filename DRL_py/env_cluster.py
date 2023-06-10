fc603fa0e8b231f73e11f2d15cab06b1516bff5f
Zan-AA <xu.zan.2011@vjc.sg>
2023-06-10 00:56:37 -0700

    minor change

--- DRL_py/env_cluster.py
+++ DRL_py/env_cluster.py
@@ -33,7 +33,7 @@ class env:
             rsh.write(f"""#!/bin/bash -l        
 #SBATCH --partition=normal,jjalonso,owners
 #SBATCH --nodes=1
-#SBATCH --time=01:00:00
+#SBATCH --time=00:05:00
 #SBATCH --job-name={job_name}
 #SBATCH --ntasks-per-node=4
 
@@ -97,7 +97,7 @@ singularity run of2206-py1.12.1-cpu.sif {file} {job_dir}""")
                  f'sed -i "/^endTime/ s/endTime.*/endTime         {endtime};/g" {traj_path}/system/controlDict &&'
                  f'sed -i "s/timeStart.*/timeStart       {rand_control_traj[0]};/g" {traj_path}/system/controlDict')
 
-        self.write_jobfile(job_name=f'traj_{buffer_counter}', file=traj_path+'/Allrun', job_dir=traj_path+'/')
+        self.write_jobfile(job_name=f'traj_{buffer_counter}', file='. /usr/lib/openfoam/openfoam2206/etc/bashrc && ' + traj_path+'/Allrun', job_dir=traj_path+'/')
         jobfile_path = f'{traj_path}' + '/jobscript.sh'
 
         proc[buffer_counter] = subprocess.Popen(['sh', 'submit_job.sh', jobfile_path])
